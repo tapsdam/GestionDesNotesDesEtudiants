abd16732a010228089ac61a84b76a3a860767cd1
1HassanAbdi <hassanabdigaleb@gmail.com>
2021-04-10 23:34:18 -0400

    structure initiale du projet

---
+++ ProjetAssauranceQualite/ProjetAssauranceQualite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetAssauranceQualite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
