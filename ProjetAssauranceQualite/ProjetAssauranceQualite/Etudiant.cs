cba62a1e3c77112d3bb07a4f1e06cee2ad11bb51
1HassanAbdiG <82282318+1HassanAbdiG@users.noreply.github.com>
2021-04-15 19:00:37 -0400

    Update Etudiant.cs

--- ProjetAssauranceQualite/ProjetAssauranceQualite/Etudiant.cs
+++ ProjetAssauranceQualite/ProjetAssauranceQualite/Etudiant.cs
@@ -6,5 +6,22 @@ namespace ProjetAssauranceQualite
 {
     class Etudiant
     {
+        
+        public override string ToString()
+		{
+			return "\n\tNumero Etudiant "+this.NumeroEtudiant+"\n\tNom : "+this.Nom+"\n\tPrenom : "+this.Prenom;
+		}
+
+		public static void AfficherListeEtudiant(List<Etudiant> listeEtudiants)
+		{
+			if (listeEtudiants.Count < 1)
+			{
+				Console.WriteLine("Aucun etudiant disponible dans votre base de donnee\n");
+			}
+			foreach (Etudiant etud in listeEtudiants)
+			{
+				Console.WriteLine(etud.ToString());
+			}
+		}
     }
 }
