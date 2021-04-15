58e225a6840e5458a504c43456ae119997dc9fb9
BahDioulde <82293986+BahDioulde@users.noreply.github.com>
2021-04-15 18:42:06 -0400

    Update Etudiant.cs

--- ProjetAssauranceQualite/ProjetAssauranceQualite/Etudiant.cs
+++ ProjetAssauranceQualite/ProjetAssauranceQualite/Etudiant.cs
@@ -6,5 +6,21 @@ namespace ProjetAssauranceQualite
 {
     class Etudiant
     {
+        
+        public int NumeroEtudiant { get; set; }	  
+		public string Nom { get; set; }
+		public string Prenom { get; set; }
+
+		public Etudiant(int numeroEtudiant, string nom, string prenom)
+		{
+			this.Nom = nom;
+			this.Prenom = prenom;
+			this.NumeroEtudiant = numeroEtudiant;
+		}
+
+		public override string ToString()
+		{
+			return "\n\tNumero Etudiant "+this.NumeroEtudiant+"\n\tNom : "+this.Nom+"\n\tPrenom : "+this.Prenom;
+		}
     }
 }
